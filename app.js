7ae2f6866938a321df873465438835870e0fc9c0
Nguyen <nguyen.dang@evolus.vn>
2018-04-11 16:04:33 +0700

    implement less render, materialdesignicons

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-const path          = require("path");
-const fs            = require("fs");
-const os            = require("os");
+const {app, protocol, shell, BrowserWindow} = require("electron");
+const path = require("path");
+const fs = require("fs");
+const os = require("os");
+
+let less = require("less");
+let mainWindow = null;
+let currentScript = {};
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        title: "electron quick start",
+        icon: "images/app_icon.png",
+        autoHideMenuBar: true
+    });
+    var context = window;
+    less.logger.addListener({
+        debug: function(msg) {
+            console.log("[LESS]" + msg);
+        },
+        info: function(msg) {
+            console.info("[LESS]" + msg);
+        },
+        warn: function(msg) {
+            console.warn("[LESS]" + msg);
+        },
+        error: function(msg) {
+            console.error("[LESS]" + msg);
+        }
+    });
+}
--- views/MainApplication.js
+++ views/MainApplication.js
@@ -1,9 +1,8 @@
 function MainApplication() {
     BaseTemplatedWidget.call(this);
-    console.log("MainApplication");
 }
 __extend(BaseTemplatedWidget, MainApplication);
 
 MainApplication.prototype.onAttached = function() {
-    console.log("MainApplication attached");
+    console.log("MainApplication is attached.");
 }
