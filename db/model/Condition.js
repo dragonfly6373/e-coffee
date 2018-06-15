4a2e554a6612ceb8db6792208578b3cbeb9c95aa
Nguyen <nguyen.dang@evolus.vn>
2018-06-15 15:51:25 +0700

    Update model Condition

--- db/model/Condition.js
+++ db/model/Condition.js
@@ -6,9 +6,11 @@ var Condition = function(query) {
 Condition.prototype = {
     and: function(condition) {
         this.queue.push({operator: " AND", condition: condition});
+        return this;
     },
     or: function(condition) {
         this.queue.push({operator: " OR", condition: condition});
+        return this;
     }
 }
 
@@ -25,38 +27,43 @@ Condition.prototype.build = function() {
     return queryStr;
 }
 
-Condition.__proto__ = {
-    eq: function(name, value) {
-        return new Condition(" " + name + " = " + value);
-    },
-    ne: function(name, value) {
-        return new Condition(" " + name + " != " + value);
-    },
-    gt: function(name, value) {
-        return new Condition(" " + name + " > " + value);
-    },
-    gte: function(name, value) {
-        return new Condition(" " + name + " >= " + value);
-    },
-    lt: function(name, value) {
-        return new Condition(" " + name + " < " + value);
-    },
-    lte: function(name, value) {
-        return new Condition(" " + name + " <= " + value);
-    },
-    in: function(name, args) {
-        return new Condition(" " + name + " IN (" + args.join(", ") + ")");
-    },
-    between: function(name, start, end) {
-        return new Condition(" " + name + " BETWEEN (" + start + ", " + end + ")");
-    },
-    isNull: function(name) {
-        return new Condition(" " + name + " IS NULL");
-    },
-    isNotNull: function(name) {
-        return new Condition(" " + name + " IS NOT NULL");
-    },
-    iLike: function(name, value) {
-        return new Condition(" " + name + " ILIKE " + value);
+Condition.__proto__ = function() {
+    function isolateDataType(data) {
+        return typeof(data) != "number" ? "'" + data + "'" : data;
     }
-}
+    return {
+        eq: function(name, value) {
+            return new Condition(" " + name + " = " + isolateDataType(value));
+        },
+        ne: function(name, value) {
+            return new Condition(" " + name + " != " + isolateDataType(value));
+        },
+        gt: function(name, value) {
+            return new Condition(" " + name + " > " + isolateDataType(value));
+        },
+        gte: function(name, value) {
+            return new Condition(" " + name + " >= " + isolateDataType(value));
+        },
+        lt: function(name, value) {
+            return new Condition(" " + name + " < " + isolateDataType(value));
+        },
+        lte: function(name, value) {
+            return new Condition(" " + name + " <= " + isolateDataType(value));
+        },
+        in: function(name, args) {
+            return new Condition(" " + name + " IN (" + args.map((item) => isolateDataType(item)).join(", ") + ")");
+        },
+        between: function(name, start, end) {
+            return new Condition(" " + name + " BETWEEN (" + isolateDataType(start) + ", " + isolateDataType(end) + ")");
+        },
+        isNull: function(name) {
+            return new Condition(" " + name + " IS NULL");
+        },
+        isNotNull: function(name) {
+            return new Condition(" " + name + " IS NOT NULL");
+        },
+        iLike: function(name, value) {
+            return new Condition(" " + name + " ILIKE " + isolateDataType(value));
+        }
+    }
+}();
