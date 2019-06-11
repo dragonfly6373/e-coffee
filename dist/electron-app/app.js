445aa4107edde6b2f633d1fcf0a2beb3016e2ca7
Nguyen <nguyen.dang@evolus.vn>
2019-06-11 16:26:56 +0700

    Rebuild sqlite native to current OS version

--- dist/electron-app/app.js
+++ dist/electron-app/app.js
@@ -2,7 +2,7 @@ const {app, protocol, shell, BrowserWindow} = require("electron");
 const path = require("path");
 const fs = require("fs");
 const os = require("os");
-// const sqlite3 = require("sqlite3").verbose();
+const sqlite3 = require('sqlite3').verbose();
 
 const less = require("less");
 const mainWindow = null;
